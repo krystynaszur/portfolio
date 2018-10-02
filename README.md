a34332862b85db1416fac8953521ea059c5d86d4
Krystyna Szurmańska <kszurmanska@gmail.com>
2018-10-02 16:57:06 +0200

    Update README.md

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Portfolio Krystyna Szurmańska
+# Portfolio of Krystyna Szurmańska - developed with HTML5, CSS3, Javascript and isotope library.
+
+https://krystynaszur.github.io/portfolio/
--- script.js
+++ script.js
@@ -27,3 +27,47 @@ function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
   }
+
+  var grid = document.querySelector('.grid');
+var iso = new Isotope(grid, {
+  itemSelector: '.grid-item',
+  layoutMode: 'fitRows',
+  masonry: {
+    isFitWidth: true
+  }
+});
+
+var filtersElem = document.querySelector('.filters-button-group');
+filtersElem.addEventListener('click', function (event) {
+  // only work with buttons
+  if (!matchesSelector(event.target, 'button')) {
+    return;
+  }
+  var filterValue = event.target.getAttribute('data-filter');
+  // use matching filter function
+  iso.arrange({ filter: filterValue });
+
+});
+
+/*
+var showMore = document.querySelector('.showMore');
+showMore.addEventListener( 'click', function( event ) {
+  var items = '<div class="grid-item">YAY</div>';
+  elem.append(items);
+  elem.isotope( 'appended', items );
+
+});*/
+
+var gridButtons = document.querySelectorAll(".grid-button");
+console.log(gridButtons);
+
+for (var i = 0; i < gridButtons.length; i++) {
+  console.log(gridButtons[i].outerText);
+  gridButtons[i].addEventListener("click", function (event) {
+    //  var activeButton = document.activeElement.id;
+    //  console.log(activeButton);
+    changeActiveButton(document.activeElement.id);
+    var showMoreButton = document.getElementById('show-more-button');
+    showMoreButton.style.display = "inline";
+  });
+};
